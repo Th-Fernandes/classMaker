bd397f4a241d07b996f893cde1cee9d9cffe98f8
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-05-23 09:16:41 -0300

    test: login flow

--- cypress/e2e/accountPage.cy.ts
+++ cypress/e2e/accountPage.cy.ts
@@ -22,4 +22,26 @@ describe('account page', () => {
       .url()
       .should('include', '/tutorial');
   });
+
+  it('should get to /home route when logged in', () => {
+    cy
+      .get('[data-cy="loginRedirectButton"]')
+      .click();
+    
+    cy
+      .get('[data-cy="emailInputLogin"]')
+      .type('joseph1987@gmail.com');
+    
+    cy
+      .get('[data-cy="passwordInputLogin"]')
+      .type('password123');
+     
+    cy
+      .get('[data-cy="loginButton"]')
+      .click();
+      
+    cy
+      .url()
+      .should('include', '/home');
+  });
 });
