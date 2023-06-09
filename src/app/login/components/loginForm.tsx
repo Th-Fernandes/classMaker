5913b002c8bf6fcacb430c3faf07cd6d07889558
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-06-09 08:50:03 -0300

    feat: login form styles + structure

--- src/app/login/components/loginForm.tsx
+++ src/app/login/components/loginForm.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import { LoginInput } from "./loginInput";
 
 export function LoginForm() {
   return (
-    <form>
-      <div>
-        <label htmlFor="emailLogin"></label>
-        <input type="text" id="emailLogin" data-cy="emailInputLogin" />
-      </div>
+    <form className="px-8 py-16 mx-3 bg-white-400 rounded-2xl flex flex-col gap-8 w-full max-w-[500px]">
+      <Image className="self-center" src="/logo-dark-full.png" alt="class maker logo" width={269} height={40}/>
 
-      <div>
-        <label htmlFor="passwordLogin"></label>
-        <input type="text" id="passwordLogin" data-cy="passwordInputLogin" />
-      </div>
-
-      <Link href="/home">
-        <button type="submit" data-cy="loginButton">Login</button>
-      </Link>
+      <LoginInput label="E-mail" testKeyWord="emailInput" id="emailInputLogin" />
+      <LoginInput label="Senha" testKeyWord="passwordInput" id="passwordInputLogin"/>
+      
+      <button
+        className="w-full py-1 font-medium text-lg text-white-400 bg-brand-700 rounded-lg focus:outline focus:outline-2 focus:outline-focus" 
+        type="submit" 
+        data-cy="loginButton"
+      >
+        <Link className="w-full h-full block" href="/home">Entrar na plataforma</Link>
+      </button>
     </form>
   );
 }
