68393161dbb999bd2a31a7975bb743bac1d67111
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-06-13 10:18:38 -0300

    feat: back to /account

--- src/app/account/tutorial/components/TutorialStepsSlider.tsx
+++ src/app/account/tutorial/components/TutorialStepsSlider.tsx
@@ -8,13 +8,13 @@ import { ChevronLeft, ChevronRight, Loader2 } from "lucide-react";
 export function TutorialStepsSlider() {
   const router = useRouter();
   const [stepPosition, setStepPosition] = React.useState<number>(0);
-  const [isNextPathLoading, setIsNextPathLoading] = React.useState<boolean>(false);
+  const [isPathLoading, setIsPathLoading] = React.useState<boolean>(false);
 
   function handleNextStep() {
     const hasAnotherStepToShow = stepPosition < tutorialSteps.length - 1;
     
     if(!hasAnotherStepToShow) {
-      setIsNextPathLoading(true);
+      setIsPathLoading(true);
       router.push('/home');
       return;
     }
@@ -23,23 +23,29 @@ export function TutorialStepsSlider() {
   }
 
   function handlePreviousStep() {
-    if(stepPosition > 0) setStepPosition(position => position - 1);
+    if(stepPosition > 0) {
+      setStepPosition(position => position - 1);
+      return;
+    }
+
+    setIsPathLoading(true);
+    router.push('/account');
   }
 
   return (
     <section className=" px-4 pb-8 text-white-400 max-w-[500px] flex flex-col gap-8 sm:gap-16 sm:pb-16 mx-auto">
-      {isNextPathLoading && <Loader2 className="mx-auto animate-spin" strokeWidth={3} size={64} />}
+      {isPathLoading && <Loader2 className="mx-auto animate-spin" strokeWidth={3} size={64} />}
       
       <div>
-        <h2 className="text-md mb-3 font-bold sm:text-lg" data-cy="stepTitle">
+        <h2 className="text-md-hd mb-3 font-bold sm:text-lg-hd" data-cy="stepTitle">
           {tutorialSteps[stepPosition].title}
         </h2>
-        <p className="text-base lg:text-sm" data-cy="stepContent">
+        <p className="text-base lg:text-sm-hd" data-cy="stepContent">
           {tutorialSteps[stepPosition].content}
         </p>
       </div>
 
-      <ul className="flex justify-between text-sm sm:text-md">
+      <ul className="flex justify-between text-hd sm:text-md-hd">
         <li>
           <button className="flex gap-3 hover:text-gray-50 transition-colors" data-cy="backStepButton" onClick={handlePreviousStep}>
             <ChevronLeft strokeWidth={3} />
