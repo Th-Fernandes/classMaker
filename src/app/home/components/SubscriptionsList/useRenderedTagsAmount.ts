52bda65bc31e3df0cfde0655aaa394008454f7be
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-07-04 16:27:40 -0300

    feat: rendered tags hook

---
+++ src/app/home/components/SubscriptionsList/useRenderedTagsAmount.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function useRenderedTagsAmount() {
+  const windowWidth = React.useRef(window.innerWidth);
+
+  const tagsAmount = windowWidth.current < 600 ? 1 : 2;
+
+  return {
+    tagsAmount
+  };
+}
