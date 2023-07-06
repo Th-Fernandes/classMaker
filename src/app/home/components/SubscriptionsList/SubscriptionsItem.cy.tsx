f0a4b3bb7477c51aa4596f870b59355a0996b1c5
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-07-06 13:33:42 -0300

    feat: tags amount on bigger screens

--- src/app/home/components/SubscriptionsList/SubscriptionsItem.cy.tsx
+++ src/app/home/components/SubscriptionsList/SubscriptionsItem.cy.tsx
@@ -1,8 +1,12 @@
-import { SubscriptionsItem } from "./SubscriptionsItem";
+import { SubscriptionsItem} from "./SubscriptionsItem";
+import { useRenderedTagsAmount } from "./useRenderedTagsAmount";
 
-describe('SubscriptionsItem component', () => {
+
+
+describe('SubscriptionsItem component on mobile', () => {
   beforeEach(() => {
-    cy.mount(<SubscriptionsItem title="Lorem Ipsum" tags={['HTML', 'CSS']}/>);
+    cy.viewport(360, 750);
+    cy.mount(<SubscriptionsItem title="Lorem Ipsum" tags={['HTML', 'CSS', 'js']}/>);
   });
 
   it('should render a single tag when on mobile', () => {
@@ -12,5 +16,19 @@ describe('SubscriptionsItem component', () => {
       .get('[data-cy="subscriptionsItemTags"]')
       .find('li')
       .should('have.length', 1);
+  });  
+});
+
+describe('SubscriptionsItem component on tablet and desktop', () => {
+  beforeEach(() => {
+    cy.viewport(1024, 900);
+    cy.mount(<SubscriptionsItem title="Lorem Ipsum" tags={['HTML', 'CSS', 'js']}/>);
+  });
+
+  it('should render more than a tag when on bigger screens', () => {
+    cy
+      .get('[data-cy="subscriptionsItemTags"]')
+      .find('li')
+      .should('have.length', 2);
   });
 });
