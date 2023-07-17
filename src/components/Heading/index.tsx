e428aa6cd4324fd6efff6ccbfea8efb100178192
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-07-17 15:54:15 -0300

    fix: class names do not overwrites the old ones anymore

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,15 +7,15 @@ interface Props extends HTMLAttributes<HTMLOrSVGElement> {
 
 const classFontStyles = {
   h1: "text-lg-hd",
-  h2: "text-md-hd",
+  h2: "text-md-hd lg:text-3xl",
   h3: "text-sm-hd"
 };
 
-export function Heading({ as: Tag, children, ...props }:Props) {
+export function Heading({ as: Tag, children, className, ...props }:Props) {
   if(!children) throw new Error('Heading component must have a children');
 
   return (
-    <Tag className={classFontStyles[Tag]} {...props}>
+    <Tag className={`${classFontStyles[Tag]} ${className}`} {...props}>
       {children}
     </Tag>
   );
